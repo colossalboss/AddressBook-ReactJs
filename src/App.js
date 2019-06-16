2954955c1be1d7b2a10d4fb8aeff32a154d80c5b
Ogbara G. Godstar <colossalboss@users.noreply.github.com>
2019-06-16 15:59:27 +0100

    Initialize rep

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => (
+  <div className="App">
+    <h1>Hey! How's React?</h1>
+  </div>
+)
+
+
+export default App;
